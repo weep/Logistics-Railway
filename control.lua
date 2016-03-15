a3daea6b9e658871e26f29a7a4253cd39753963f
TheAIBot <andreasuranus@hotmail.com>
2016-03-15 07:28:18 +0100

    Refractoring

--- control.lua
+++ control.lua
@@ -2,70 +2,50 @@ require "util"
 require "defines"
 
 script.on_event(defines.events.on_built_entity, function(event)
-	local extradummy = event.created_entity.surface.find_entity("requester-rail-dummy-chest", event.created_entity.position)
-	if extradummy then
-		extradummy.destroy()
-	end
-	if event.created_entity.name == "requester-rail" then
-		local dummy = event.created_entity.surface.create_entity({name = "requester-rail-dummy-chest", position = event.created_entity.position, force = event.created_entity.force})
-		dummy.insert{name="dummy-item", count=1}
-	end
+	createDummyChest(event)
 end)
 
 script.on_event(defines.events.on_robot_built_entity, function(event)
-	local extradummy = event.created_entity.surface.find_entity("requester-rail-dummy-chest", event.created_entity.position)
-	if extradummy then
-		extradummy.destroy()
-	end
+	createDummyChest(event)
+end)
+
+function createDummyChest(event)
+	removeDummy(event.created_entity.surface, event.created_entity.position, "requester-rail-dummy-chest")
 	if event.created_entity.name == "requester-rail" then
 		local dummy = event.created_entity.surface.create_entity({name = "requester-rail-dummy-chest", position = event.created_entity.position, force = event.created_entity.force})
 		dummy.insert{name="dummy-item", count=1}
 	end
-end)
+end
 
 script.on_event(defines.events.on_preplayer_mined_item, function(event)
 	if event.entity.name == "requester-rail-dummy-chest" then
-		local dummy = event.entity.surface.find_entity("requester-rail", event.entity.position)
-		if dummy then
-			dummy.destroy()
-		end
+		removeDummy(event.entity.surface, event.entity.position, "requester-rail")
 		return event.entity.clear_items_inside()
 	end
 	if event.entity.name == "requester-rail" then
-		local dummy = event.entity.surface.find_entity("requester-rail-dummy-chest", event.entity.position)
-		if dummy then
-			dummy.destroy()
-		end
+		removeDummy(event.entity.surface, event.entity.position, "requester-rail-dummy-chest")
 	end
 end)
 
 script.on_event(defines.events.on_entity_died, function(event)
 	if event.entity.name == "requester-rail-dummy-chest" then
-		local dummy = event.entity.surface.find_entity("requester-rail", event.entity.position)
-		if dummy then
-			dummy.destroy()
-		end
-		return event.entity.clear_items_inside()
+		removeDummy(event.entity.surface, event.entity.position, "requester-rail")
+		event.entity.clear_items_inside()
 	end
 end)
 
 script.on_event(defines.events.on_robot_pre_mined, function(event)
 	if event.entity.name == "requester-rail" then
-		local dummy = event.entity.surface.find_entity("requester-rail-dummy-chest", event.entity.position)
-		if dummy then
-			dummy.destroy()
-		end
+		removeDummy(event.entity.surface, event.entity.position, "requester-rail-dummy-chest")
 	end
 end)
 
-script.on_event(defines.events.on_entity_died, function(event)
-	if event.entity.name == "requester-rail-dummy-chest" then
-		local dummy = event.entity.surface.find_entity("requester-rail", event.entity.position)
-		if dummy then
-			dummy.destroy()
-		end
+function removeDummy(surface, position, typeToRemove)
+	local dummy = surface.find_entity(typeToRemove, position)
+	if dummy then
+		dummy.destroy()
 	end
-end)
+end
 
 script.on_event(defines.events.on_train_changed_state, function(event)
 	if event.train.state == defines.trainstate.wait_station and event.train.speed == 0 then -- Wagons only interact with the logistics network when they are waiting at a station in automatic mode
@@ -154,38 +134,23 @@ function syncChests(train)
 			local passive_provider = wagon.surface.find_entity("passive-provider-chest-from-wagon", wagon.position)
 			local active_provider = wagon.surface.find_entity("active-provider-chest-from-wagon", wagon.position)
 			local storage = wagon.surface.find_entity("storage-chest-from-wagon", wagon.position)
-			if requester then
-				local wagon_inventory = wagon.get_inventory(defines.inventory.chest)
-				wagon_inventory.setbar()
-				copyInventory(requester, wagon) -- Chest to wagon
-				requester.destroy()
-				wagon.minable = true
-				wagon.operable = true
-			end
-			if passive_provider then
-				local wagon_inventory = wagon.get_inventory(defines.inventory.chest)
-				wagon_inventory.setbar()
-				copyInventory(passive_provider, wagon) -- Chest to wagon
-				passive_provider.destroy()
-				wagon.minable = true
-				wagon.operable = true
-			end
-			if active_provider then
-				local wagon_inventory = wagon.get_inventory(defines.inventory.chest)
-				wagon_inventory.setbar()
-				copyInventory(active_provider, wagon) -- Chest to wagon
-				active_provider.destroy()
-				wagon.minable = true
-				wagon.operable = true
-			end
-			if storage then
-				local wagon_inventory = wagon.get_inventory(defines.inventory.chest)
-				wagon_inventory.setbar()
-				copyInventory(storage, wagon) -- Chest to wagon
-				storage.destroy()
-				wagon.minable = true
-				wagon.operable = true
-			end
+			prepareForTrainDepartment(requester, wagon)
+			prepareForTrainDepartment(passive_provider, wagon)
+			prepareForTrainDepartment(active_provider, wagon)
+			prepareForTrainDepartment(storage, wagon)
 		end
 	end
 end
+
+function prepareForTrainDepartment(chestEntity, wagon)
+	if chestEntity then
+		local wagon_inventory = wagon.get_inventory(defines.inventory.chest)
+		wagon_inventory.setbar()
+		copyInventory(chestEntity, wagon) -- Chest to wagon
+		chestEntity.destroy()
+		wagon.minable = true
+		wagon.operable = true
+	end
+end
+
+
