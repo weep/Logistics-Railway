5669e1dfa51848f70e98b2bcbf7de56908a0b9bf
Suprcheese <l33tcheese@yahoo.com>
2016-03-16 15:03:06 -0700

    Release 1.0.3: Fixed an issue which was causing vanilla straight rails to be unable to cross each other.

--- prototypes/rails.lua
+++ prototypes/rails.lua
@@ -1,5 +1,3 @@
-data.raw["straight-rail"]["straight-rail"].fast_replaceable_group = "rails"
-
 local storage_rail = util.table.deepcopy(data.raw["straight-rail"]["straight-rail"])
 local passive_provider_rail = util.table.deepcopy(data.raw["straight-rail"]["straight-rail"])
 local active_provider_rail = util.table.deepcopy(data.raw["straight-rail"]["straight-rail"])
@@ -9,11 +7,6 @@ storage_rail.name = "storage-rail"
 passive_provider_rail.name = "passive-provider-rail"
 active_provider_rail.name = "active-provider-rail"
 requester_rail.name = "requester-rail"
-
-storage_rail.fast_replaceable_group = "rails"
-passive_provider_rail.fast_replaceable_group = "rails"
-active_provider_rail.fast_replaceable_group = "rails"
-requester_rail.fast_replaceable_group = "rails"
 
 storage_rail.flags = {"placeable-neutral", "player-creation"}
 passive_provider_rail.flags = {"placeable-neutral", "player-creation"}
@@ -25,7 +18,7 @@ passive_provider_rail.collision_box = {{-0.7, -0.99}, {0.7, 0.99}}
 active_provider_rail.collision_box = {{-0.7, -0.99}, {0.7, 0.99}}
 requester_rail.collision_box = {{-0.7, -0.99}, {0.7, 0.99}}
 
--- requester_rail.selection_box = {{-0.1, -0.1}, {0.1, 0.1}} -- This doesn't seem to actually work
+-- requester_rail.selection_box = {{-0.1, -0.1}, {0.1, 0.1}} -- This doesn't seem to actually work either
 
 storage_rail.pictures.straight_rail_vertical.metals.tint = { r = 1.0, g = 1.0, b = 0.0, a = 0.5 }
 storage_rail.pictures.straight_rail_diagonal.metals.tint = { r = 1.0, g = 1.0, b = 0.0, a = 0.5 }
